0ea80e0b76f2d50b8935a64cc1031680ac959203
Taufiq287 <natanhyper32@gmail.com>
2024-11-21 11:27:46 +0700

    MEMBUAT PROGRAM MQ2 DIGITAL OUTPUT SERIAL MONITOR
    saat program dijalankan sensor akan mendeteksi gas apakah terdapat gas atau tidak bila tidak serial monitor akan memunculkan teks "gas tidak terdeteksi" bila ada gas maka serial monitor akan menunculkan teks "gas terdeteksi"

---
+++ TAUFIQ_K.M_LKPD_13.ino
@@ -0,0 +1,17 @@
+//TAUFIQ KAMAL MAHMUD
+//XI TAV-1
+#define MQ2pin 16 // Mendefinisikan pin yang terhubung ke sensor MQ2
+int sensorValue; // Variabel untuk menyimpan nilai sensor
+void setup() {
+  Serial.begin(9600); // Mulai komunikasi serial dengan kecepatan 9600 baud
+  Serial.println("Memanaskan MQ2"); // Cetak pesan "Memanaskan MQ2" ke port serial
+  delay(20000);}
+void loop() {
+  sensorValue = digitalRead(MQ2pin);
+  Serial.print("Digital Output: ");
+  Serial.print(sensorValue);
+  if (sensorValue) {
+    Serial.println(" | Smoke/Gas: Tidak Terdeteksi");
+  } else {
+    Serial.println(" | Smoke/Gas: Terdeteksi!");}
+  delay(2000);}
